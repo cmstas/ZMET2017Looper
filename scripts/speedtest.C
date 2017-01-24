bf342a305c1f78d626ea2000ff291273cc10fb53
Bobak Hashemi <bth5032@gmail.com>
2017-01-23 17:17:06 -0800

    testing python stuff for smsscanner

---
+++ scripts/speedtest.C
@@ -0,0 +1,25 @@
+void check(){
+  TChain *ch = new TChain("t");
+
+  ch->Add("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-07/t5zz_80x_v2.root");
+
+  int n_entries = ch->GetEntries();
+  int i_entry, a, b;
+  double nb;
+  for (int j_entry = 0; j_entry < n_entries; j_entry++){
+    i_entry = ch->LoadTree(j_entry);
+    if (i_entry < 0) break;
+    nb = ch->GetEntry(j_entry);
+    if (nb <= 0) continue;
+    if (j_entry % 10000 == 0){
+      cout<<"Processing entry "<<j_entry<<" of "<<n_entries<<endl;
+    }
+
+    a=ch.mass_gluino;
+    b=ch.mass_LSP;
+  }
+}
+
+void speedtest(){
+  check();
+}
