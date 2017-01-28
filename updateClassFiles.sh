43cb6bd25ab4e35a35c743e87c99a3134dfca8e5
Bobak Hashemi <bth5032@gmail.com>
2017-01-27 18:57:02 -0800

    trying to figure out class files thing

--- updateClassFiles.sh
+++ updateClassFiles.sh
@@ -1,7 +1,17 @@
-pushd /home/users/bhashemi/Projects/GIT/Software/
-git pull 
-git checkout root6
-cd makeCMS3ClassFiles
-root -l -b -q "makeCMS3ClassFiles.C(\"/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-09/skims/gjetsht400_dr0p4_mgmlm.root\", \"t\", \"ZMET2016\", \"zmet\", \"phys\")"
-mv ZMET2016.* /home/users/bhashemi/Projects/GIT/ZMETBabyLooper2017/
-popd
+if [[ $# < 1 ]]
+then
+  echo "./updateClassFiles.sh /path/to/example/baby.root"
+  read -e -p "Glassfish Path:" GF_DIR
+elif [[ ! -a $1 ]]
+then
+  echo "The file $1 could not be found."
+  echo "./updateClassFiles.sh /path/to/example/baby.root"
+else
+  pushd /home/users/bhashemi/Projects/GIT/Software/
+  git pull 
+  git checkout root6
+  cd makeCMS3ClassFiles
+  root -l -b -q "makeCMS3ClassFiles.C(\"/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-09/skims/gjetsht400_dr0p4_mgmlm.root\", \"t\", \"ZMET2016\", \"zmet\", \"phys\")"
+  mv ZMET2016.* /home/users/bhashemi/Projects/GIT/ZMETBabyLooper2017/
+  popd
+fi
