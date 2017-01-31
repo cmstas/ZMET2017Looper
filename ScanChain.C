cbbb78f543d2fb02c7da02bb581695c14ca2631a
Bobak Hashemi <bth5032@gmail.com>
2017-01-31 10:03:36 -0800

    added print statement for high weight events

--- ScanChain.C
+++ ScanChain.C
@@ -1889,6 +1889,10 @@ int ScanChain( TChain* chain, ConfigParser *configuration, bool fast/* = true*/,
       double weight = getWeight();
       weight_log->Fill(log10(abs(weight)));
       weight_log_flat->Fill(abs(weight));
+
+      if (phys.met_T1CHS_miniAOD_CORE_pt() > 150 && phys.met_T1CHS_miniAOD_CORE_pt() < 250){
+        cout<<"evt: "<<phys.evt()<<" run: "<<phys.run()<<" lumi: "<<phys.lumi()<<" weight: "<<weight()<<endl;
+      }
 //===========================================
 // Analysis Code
 //===========================================
