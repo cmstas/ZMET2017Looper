689018565dd3ed6287869c8fdd16be8ad7449319
Bobak Hashemi <bth5032@gmail.com>
2017-02-01 16:07:43 -0800

    removed useless trace statements and made selection for the one TChiHZ event more exclusive

--- ScanChain.C
+++ ScanChain.C
@@ -1302,7 +1302,7 @@ bool passFileSelections(){
         numEvents->Fill(68);
         return false;
       }
-      if (conf->get("signal_region") == "TChiHZ" && phys.evt() == 24645544){
+      if (conf->get("signal_region") == "TChiHZ" && phys.evt() == 24645544 && TString(conf->get("conf_path")).Contains("TemplatesClosure") ){
         cout<<"Hand removed the one TChiHZ event"<<endl;
         return false;
       }
@@ -1894,7 +1894,7 @@ int ScanChain( TChain* chain, ConfigParser *configuration, bool fast/* = true*/,
       weight_log->Fill(log10(abs(weight)));
       weight_log_flat->Fill(abs(weight));
 
-      if (phys.met_T1CHS_miniAOD_CORE_pt() > 120 && conf->get("signal_region") == "Strong_Btag_6j"  && weight > 0.1){
+      /*if (phys.met_T1CHS_miniAOD_CORE_pt() > 120 && conf->get("signal_region") == "Strong_Btag_6j"  && weight > 0.1){
         cout<<"evt: "<<phys.evt()<<" run: "<<phys.run()<<" lumi: "<<phys.lumi()<<" weight: "<<weight<<endl;
       }
       if (phys.met_T1CHS_miniAOD_CORE_pt() > 120 && conf->get("signal_region") == "Strong_Bveto_6j" && weight > 0.1){
@@ -1902,7 +1902,7 @@ int ScanChain( TChain* chain, ConfigParser *configuration, bool fast/* = true*/,
       }
       if (phys.met_T1CHS_miniAOD_CORE_pt() > 100 && phys.met_T1CHS_miniAOD_CORE_pt() < 150 && conf->get("signal_region") == "TChiHZ"){
         cout<<"evt: "<<phys.evt()<<" run: "<<phys.run()<<" lumi: "<<phys.lumi()<<" weight: "<<weight<<endl;
-      }
+      }*/
 
 //===========================================
 // Analysis Code
