44fafab0bc9eb17e3a951cfb3a4998f4f64b9fe4
Bobak Hashemi <bth5032@gmail.com>
2017-02-11 19:18:45 +0100

    added better scale factor logic for lepton SFs, fixed double counting of trigger efficiency, added TH3Ds for flucuations up and down of btag and ISR SFs in SUSY scans

--- ScanChain.C
+++ ScanChain.C
@@ -1726,7 +1726,9 @@ int ScanChain( TChain* chain, ConfigParser *configuration, bool fast/* = true*/,
   TH3D *susy_type1MET_counts;
 
   if(conf->get("SUSY_Glu_LSP_scan") == "true"){
-    susy_MET_counts = new TH3D("susy_type1MET_counts", "(x,y,z) = (met, m_glu, m_lsp). Type1MET for"+g_sample_name, 6000, 0, 6000, 3000, 0, 3000, 3000, 0, 3000);
+    susy_type1MET_counts = new TH3D("susy_type1MET_counts", "(x,y,z) = (met, m_glu, m_lsp). Type1MET for"+g_sample_name, 6000, 0, 6000, 3000, 0, 3000, 3000, 0, 3000);
+    susy_type1MET_counts->SetDirectory(rootdir);
+    susy_type1MET_counts->Sumw2();
 
     TH3D *susy_type1MET_btaglight_up = new TH3D("susy_type1MET_btaglight_up", " (x,y,z) = (met, m_glu, m_lsp). Type 1 MET with Light Btag SF fluctuated up for"+g_sample_name, 6000, 0, 6000, 3000, 0, 3000, 3000, 0, 3000);
     susy_type1MET_btaglight_up->SetDirectory(rootdir);
