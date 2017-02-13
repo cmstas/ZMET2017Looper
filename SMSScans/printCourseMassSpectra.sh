951ec2fa3734444121c9170ab5113a95b33612de
Bobak Hashemi <bth5032@gmail.com>
2017-02-13 17:27:14 +0100

    fixed mass spectrum in datacard maker

---
+++ SMSScans/printCourseMassSpectra.sh
@@ -0,0 +1,24 @@
+spectra_file="SMSScans/Spectra/mass_spectrum_t5zz_orig.txt"
+
+
+n_glu_points=`cat $spectra_file | awk '{print $3}' | sort -n | uniq | wc -l`
+glu_points=`cat $spectra_file | awk '{print $3}' | sort -n | uniq | xargs | tr ' ' ','`
+#echo $n_glu_points
+
+#echo "const int n_gluino_bins = $((n_glu_points-1));"
+#echo "const double gluino_bins[n_gluino_bins+1]={$glu_points};"
+
+
+n_lsp_points=`cat $spectra_file | awk '{print $5}' | grep -v "40\." | grep -v "90\." | sort -n | uniq | tail -n+3 | wc -l`
+lsp_points=`cat $spectra_file | awk '{print $5}' | grep -v "40\." | grep -v "90\." | sort -n | uniq | tail -n+3 | xargs | tr ' ' ','`
+#echo $n_lsp_points
+
+#echo "const int n_lsp_bins = $((n_lsp_points-1));"
+#echo "const double lsp_bins[n_lsp_bins+1]={$lsp_points};"
+
+mp=`cat $spectra_file | awk '{print "("$3","$5")"}' | grep -v "40\." | grep -v "90\." | grep -v ",25\." | grep -v ",50\." | xargs | tr ' ' ','`
+
+echo "mass_spectrum=[$mp]"
+
+
+
