95ef2660e67a94a8cbe25870e9264b2771050527
Bobak Hashemi <bth5032@gmail.com>
2017-02-21 15:57:35 +0100

    set up for getting leonora's event lists

--- ScanChain.C
+++ ScanChain.C
@@ -2049,14 +2049,14 @@ int ScanChain( TChain* chain, ConfigParser *configuration, bool fast/* = true*/,
       printFail = false;
 
       //if (inspection_set.count(phys.evt()) != 0){
-      if ( inspection_set_erl.count(make_tuple(phys.evt(), phys.run(), phys.lumi())) != 0){
+      /*if ( inspection_set_erl.count(make_tuple(phys.evt(), phys.run(), phys.lumi())) != 0){
         cout<<"evt: "<<phys.evt()<<" run: "<<phys.run()<<" lumi: "<<phys.lumi()<<endl;
         printStats=true;
         printFail=true;
       }
       else{
         continue;
-      }
+      }*/
       
       /*if ( inVinceNotMine.count(phys.evt()) != 0){
         printFail = true;
