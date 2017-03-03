c6a4c2358c96e4db22ff0ad500c185bb5e3329a2
Bobak Hashemi <bth5032@gmail.com>
2017-03-03 20:09:12 +0100

    moved to t5zz binning for signal scan

--- ScanChain.C
+++ ScanChain.C
@@ -668,7 +668,7 @@ double getReweight(){
       }
     }
     else{
-      std::string error = "Reweight varible is not a valid option, please choose vpt, or ht_wide, got: "+rwt_var.Data();
+      std::string error = TString("Reweight varible is not a valid option, please choose vpt, or ht_wide, got: "+rwt_var.Data()).Data();
       throw std::invalid_argument(error);
     }
   }
