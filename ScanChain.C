07d77606f50f8ea984017a5d8c3360a6ef4d9bcc
Bobak Hashemi <bth5032@gmail.com>
2017-03-06 16:06:06 +0100

    added extra weight output

--- ScanChain.C
+++ ScanChain.C
@@ -2305,7 +2305,7 @@ int ScanChain( TChain* chain, ConfigParser *configuration, bool fast/* = true*/,
           inspection_copy.erase(make_tuple(phys.evt(), phys.run(), phys.lumi()));
         }*/
         //When Debug mode is off, you can turn this on:
-        cout<<"evt: "<<phys.evt()<<" run: "<<phys.run()<<" lumi: "<<phys.lumi()<<endl;
+        cout<<"evt: "<<phys.evt()<<" run: "<<phys.run()<<" lumi: "<<phys.lumi()<<" scale1fb: "<<phys.evt_scale1fb()<<" weight: "<<weight<<" extra_weight: "<< weight/phys.evt_scale1fb() <<endl;
       }
 //===========================================
 // Analysis Code
