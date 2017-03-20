57bef0ee55e65b977ea3e5ea8ee4d74ea496dee4
Bobak Hashemi <bth5032@gmail.com>
2017-03-20 14:38:39 -0700

    fussing with debugging script

--- scripts/getEventsNotInBabies.sh
+++ scripts/getEventsNotInBabies.sh
@@ -1,20 +1,20 @@
 function getEventsNotInBabies {
+  MAXNUM=50
   num_low=`cat $1 | grep -n "Events that weren't in your babies" | cut -d':' -f1`
   num_high=`cat $1 | grep -n "Num events passed" | cut -d':' -f1`
 
   num_low=$((num_low+1))
   num_high=$((num_high-1))
 
-  while [[ $((num_low+30)) -le $num_high ]]
+  while [[ $(($num_low+$MAXNUM)) -le $num_high ]]
   do
     echo -n "ch->Scan(\"evt_event:evt_run:evt_lumiBlock\", \""
-    echo `cat $1 | sed -n "${num_low},$((num_low+30))p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs` "\")"
-    num_low=$((num_low+30))
+    echo `cat $1 | sed -n "${num_low},$(($num_low+$MAXNUM))p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs` "\")"
+    num_low=$(($num_low+$MAXNUM))
   done
   
   echo -n "ch->Scan(\"evt_event:evt_run:evt_lumiBlock\", \""
-  cat $1 | sed -n "${num_low},${num_high}p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs
-  echo -n "\")"
+  echo `cat $1 | sed -n "${num_low},${num_high}p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs` "\")"
 
 }
 
