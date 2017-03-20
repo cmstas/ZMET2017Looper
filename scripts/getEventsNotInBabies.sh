12234e5e39df987497721b88d93553af0cbe9c2c
Bobak Hashemi <bth5032@gmail.com>
2017-03-20 14:58:04 -0700

    fussing with debugging script

--- scripts/getEventsNotInBabies.sh
+++ scripts/getEventsNotInBabies.sh
@@ -6,28 +6,34 @@ function getEventsNotInBabies {
   num_low=$((num_low+1))
   num_high=$((num_high-1))
 
+  echo "TChain *ch = new TChain(\"Events\");"
+  echo "ch->Add(\"$2\");"
+
   while [[ $(($num_low+$MAXNUM)) -le $num_high ]]
   do
     echo -n "ch->Scan(\"evt_event:evt_run:evt_lumiBlock\", \""
-    echo `cat $1 | sed -n "${num_low},$(($num_low+$MAXNUM))p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs` "\")"
+    echo `cat $1 | sed -n "${num_low},$(($num_low+$MAXNUM))p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs` "\");" | sed 's/|| ")/")/g'
     num_low=$(($num_low+$MAXNUM))
-    echo "======================================================="
   done
   
   echo -n "ch->Scan(\"evt_event:evt_run:evt_lumiBlock\", \""
-  echo `cat $1 | sed -n "${num_low},${num_high}p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs` "\")"
+  echo `cat $1 | sed -n "${num_low},${num_high}p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs` "\");" | sed 's/|| ")/")/g'
 
 }
 
 if [[ $# < 1 ]]
 then
-  echo "getEventsNotInBabies path/to/output/of/single/sample"
+  echo "getEventsNotInBabies path/to/output/of/single/sample /hadoop/path/to/CMS3"
   return 1
 elif [[ ! -a $1 ]]
 then
   echo "Could not find file: $1"
   return 1
+elif [[ ! -d $2 ]]
+then
+  echo "Could not find hadoop dir: $2"
+  return 1
 else
-  getEventsNotInBabies $1
+  getEventsNotInBabies $1 $2
 fi
 
