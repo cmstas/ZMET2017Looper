0df90bdf1a3659250da180b739a1978c988f6a9e
Bobak Hashemi <bth5032@gmail.com>
2017-03-20 14:27:38 -0700

    added beggining of script to get events that were not in my babies during debugging

--- scripts/getEventsNotInBabies.sh
+++ scripts/getEventsNotInBabies.sh
@@ -2,7 +2,21 @@ function getEventsNotInBabies {
   num_low=`cat $1 | grep -n "Events that weren't in your babies" | cut -d':' -f1`
   num_high=`cat $1 | grep -n "Num events passed" | cut -d':' -f1`
 
-  sed -n '${num_low},${num_high}p'
+  num_low=$((num_low+1))
+  num_high=$((num_high-1))
+
+  while [[ $((num_low+30)) -leq $num_high ]]
+  do
+    echo -n "ch->Scan(\"evt_event:evt_run:evt_lumiBlock\", \""
+    cat $1 | sed -n "${num_low},$((num_low+30))p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs
+    echo -n "\")"
+    num_low=$((num_low+30))
+  done
+  
+  echo -n "ch->Scan(\"evt_event:evt_run:evt_lumiBlock\", \""
+  cat $1 | sed -n "${num_low},${num_high}p" | awk '{print " (evt_event==" $2 " && evt_run==" $4 " && evt_lumiBlock==" $6 " ) ||"}' | xargs
+  echo -n "\")"
+
 }
 
 if [[ $# < 1 ]]
