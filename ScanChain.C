1150550aa59c559c4d6446dd5889cc264e84da09
Bobak Hashemi <bth5032@gmail.com>
2017-05-08 18:59:54 -0700

    fixed HZ BR scan bug

--- ScanChain.C
+++ ScanChain.C
@@ -812,7 +812,7 @@ double getWeight(){
   }
 
   if (conf->get("tchihz_tchizz_weightfix") == "true"){
-    double H_BR = (conf->get("tchihz_H_BR") != "") ? 0.5 : stod(conf->get("tchihz_H_BR"));
+    double H_BR = (conf->get("tchihz_H_BR") == "" ) ? 0.5 : stod(conf->get("tchihz_H_BR"));
     double Z_BR = 1-H_BR;
     if (TString(currentFile->GetTitle()).Contains("tchihz_80x_v2")){
       weight *= 2*H_BR*Z_BR; //twice the BR to HZ since we have both HZ and ZH.
