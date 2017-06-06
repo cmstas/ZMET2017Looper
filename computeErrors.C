c34be90600037708c68d4300cc8165a214a71797
Bobak Hashemi <bth5032@gmail.com>
2017-06-06 09:26:02 -0700

    playing with legend size

--- computeErrors.C
+++ computeErrors.C
@@ -303,7 +303,7 @@ void printErrors(const vector<double> &temp_err, const vector<double> &rare_err,
     cout<<bin_low[i]<<"-"<<bin_low[i+1]<<" ";
   }
   cout<<endl;
-  cout<<"Template ";
+  cout<<"Z+Jets ";
   for (int i = 0; i<temp_err.size(); i++){
     cout<<"+/-"<<temp_err[i]<<" ";
   }
@@ -356,7 +356,7 @@ void printCounts(const vector<double> &temp_count, const vector<double> &temp_er
     cout<<bin_low[i].first<<"-"<<bin_low[i].second<<" ";
   }
   cout<<endl;
-  cout<<"STATTABLE: Template ";
+  cout<<"STATTABLE: Z+Jets ";
   for (int i = 0; i<temp_err.size(); i++){
     cout<<temp_count[i]<<"+/-"<<temp_err[i]<<" ";
   }
@@ -401,7 +401,7 @@ void printLatexCounts(const vector<double> temp_count, const vector<double> &tem
   }
   cout<<" \\\\ \\hline "<<endl;
   cout<<setprecision(1);
-  cout<<"LATEXTABLE: Template ";
+  cout<<"LATEXTABLE: Z+Jets ";
   for (int i = 0; i<temp_err.size(); i++){
     cout<<" & "<<temp_count[i]<<"$\\pm$"<<temp_err[i];
   }
