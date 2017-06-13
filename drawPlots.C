6b52bdeaa6511e36524304bbbe84cb3ddabaa67f
Bobak Hashemi <bth5032@gmail.com>
2017-06-13 12:31:42 -0400

    added some plot styling stuff

--- drawPlots.C
+++ drawPlots.C
@@ -876,7 +876,7 @@ TString drawArbitraryNumberWithResidual(ConfigParser *conf){
     bg_err->Draw("SAME 2");
   }
   if (conf->get("print_stats") == "true" && conf->get("simple_errors") != "true"){
-    hists[0]->SetMarkerSize(6);
+    hists[0]->SetMarkerSize(4);
     hists[0]->SetLineWidth(5);
   }
   else{
@@ -1002,7 +1002,7 @@ TString drawArbitraryNumberWithResidual(ConfigParser *conf){
   line1->SetLineStyle(2);
 
   if (conf->get("print_stats") == "true" && conf->get("simple_errors") != "true"){
-    residual->SetMarkerSize(6);
+    residual->SetMarkerSize(4);
   }
   else{
     residual->SetMarkerSize(1.5);
