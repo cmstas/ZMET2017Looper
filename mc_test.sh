a72b5c943ec4cb2c7d8555db93f2d1487736ddc6
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-04-04 16:15:28 +0200

    Seprated EWK region

--- mc_test.sh
+++ mc_test.sh
@@ -1,4 +1,9 @@
 
 make
 
-./ZMETLooper GammaJets configs/dataMC/MC/run_modes.conf
+./ZMETLooper EWKTail_WJets configs/dataMC/MC/run_modes.conf
+./ZMETLooper EWKTail_WGamma configs/dataMC/MC/run_modes.conf
+./ZMETLooper EWKTail_ttbar2lep configs/dataMC/MC/run_modes.conf
+./ZMETLooper EWKTail_ttbar1lep configs/dataMC/MC/run_modes.conf
+./ZMETLooper EWKTail_SingleTop configs/dataMC/MC/run_modes.conf
+
