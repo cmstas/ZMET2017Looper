993eb04da7f018ecd2ca16edd77e162c1b2dd336
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-04-15 18:34:41 +0200

    added scripts to launch dilepton loopers

--- dilepton_test.sh
+++ dilepton_test.sh
@@ -1,5 +1,8 @@
-for configName in DoubleMuon DoubleElectron MuonElectron DYJets TTJets SingleTop TTZ TTW WW WZTo2L2Q WZTo3LNu ZZ2L ZZ4L
+for hyp_type in ee mumu emu
 do
-    nohup ./ZMETLooper $configName configs/2017/DoubleLepton/run_modes.conf > $configName.txt &
+    for configName in DoubleMuon DoubleElectron MuonElectron DYJets TTJets SingleTop TTZ TTW WW WZTo2L2Q WZTo3LNu ZZ2L ZZ4L
+    do
+        echo "nohup ./ZMETLooper $configName configs/2017/DoubleLepton/$hyp_type/run_modes.conf > $configName-$hyp_type.txt &"
+        nohup ./ZMETLooper $configName configs/2017/DoubleLepton/$hyp_type/run_modes.conf > $configName-$hyp_type.txt &
+    done
 done
-
