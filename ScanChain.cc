9b47b4772bcf08efa26c0b29ac8f73f493b0c651
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-04-16 23:21:05 +0200

    The amount of time it takes to remove a bunch of pesky bugs ...

--- ScanChain.cc
+++ ScanChain.cc
@@ -498,7 +498,7 @@ bool passMuonTriggers(){
       }
       else{
         
-        bool triggers = phys.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8() || phys.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ() || HLT_Mu37_TkMu27();
+        bool triggers = phys.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8() || phys.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ() || phys.HLT_Mu37_TkMu27();
         return triggers;
       }
     } 
@@ -512,7 +512,7 @@ bool passElectronTriggers(){
   else{
     //cout<<__LINE__<<endl;
     //if (printStats) { cout<<"HLT_DoubleEl_DZ_2: "<<phys.HLT_DoubleEl_DZ_2()<<" HLT_DoubleEl_noiso: "<<phys.HLT_DoubleEl_noiso()<<" HLT_DoubleEl_DZ(): "<<phys.HLT_DoubleEl_DZ()<<endl; }
-    bool eleTriggers = phys.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL() || phys.HLT_DoubleEle33_CaloIdL_MW() || phys.HLT_DoubleEle25_CaloIdL_MW() || HLT_DoubleEle27_CaloIdL_MW();
+    bool eleTriggers = phys.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL() || phys.HLT_DoubleEle33_CaloIdL_MW() || phys.HLT_DoubleEle25_CaloIdL_MW() || phys.HLT_DoubleEle27_CaloIdL_MW_Edge();
     return eleTriggers;
   }
 }
