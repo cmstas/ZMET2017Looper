dc1a448ac9f31bd7f2a0ea85e45be99025a1df66
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-05-06 17:33:08 +0200

    FML!

--- ScanChain.cc
+++ ScanChain.cc
@@ -985,7 +985,7 @@ double ZMETLooper::getWeight(){
     
     //cout<<__LINE__<<endl;
     
-    if (TString(conf->get("name")).Contains("EWKSub")){
+    if (TString(conf->get("Name")).Contains("EWKSub")){
       weight *= -1; //EWK Subtraction
     }
   }
