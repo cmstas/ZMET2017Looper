fe31ef22b23776db565291322afa20d10e5bc7c5
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-05-20 14:56:15 +0200

    fixed errors

--- ScanChain.cc
+++ ScanChain.cc
@@ -1355,15 +1355,18 @@ bool ZMETLooper::passSignalRegionCuts(){
       return false;
     }
 
-    if (conf->get("MT2_max") != ""){
-    if (g_mt2 > stod(conf->get("MT2_max"))){
+    
+  }
+
+if (conf->get("MT2_max") != ""){
+    if (g_mt2 < stod(conf->get("MT2_max"))){
       numEvents->Fill(59);
       if (printFail) cout<<phys.evt()<<" :Failed MT2 cut"<<endl;
       return false;
     }
 
+    
   }
-
 
   //cout<<__LINE__<<endl;
   //
