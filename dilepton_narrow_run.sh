c60a1214de8e10b092a400babf933f6db214d072
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-05-26 16:48:12 +0200

    changed launch script

--- dilepton_narrow_run.sh
+++ dilepton_narrow_run.sh
@@ -4,23 +4,23 @@ for sampleName in ZJets GammaJets
 do
     echo "nohup ./ZMETLooper $sampleName configs/2017/DoubleLepton_narrow/MCTemplates/run_modes.conf > MCTemplates-$sampleName.out &"
 
-    nohup ./ZMETLooper $sampleName configs/2017/DoubleLepton_narrow/MCTemplates/run_modes.conf > MCTemplates-$sampleName.out &
+    nohup ./ZMETLooper $sampleName configs/2017/DoubleLepton_narrow/DoubleLepton_narrow_validation/VRA4/MCTemplates/run_modes.conf > MCTemplates-$sampleName.out &
 done
 wait
 #plot closure
 echo "Closure histograms"
 sampleName=GammaJets_reweight
-nohup ./ZMETLooper $sampleName configs/2017/DoubleLepton_narrow/MCTemplates/run_modes.conf > MCTemplates-$sampleName.out &
+nohup ./ZMETLooper $sampleName configs/2017/DoubleLepton_narrow/DoubleLepton_narrow_validation/VRA4/MCTemplates/run_modes.conf > MCTemplates-$sampleName.out &
 
-mkdir -p ../hists2017/DoubleLepton_narrow/dataTemplates
+mkdir -p ../hists2017/DoubleLepton_narrow/DoubleLepton_narrow_validation/VRA4/dataTemplates
 for sampleName in ZJets GammaJets
 do
-    cp ../hists2017/DoubleLepton_narrow/MCTemplates/$sampleName.root ../hists2017/DoubleLepton_narrow/dataTemplates/$sampleName\_MC.root
+    cp ../hists2017/DoubleLepton_narrow/DoubleLepton_narrow_validation/VRA4/MCTemplates/$sampleName.root ../hists2017/DoubleLepton_narrow/DoubleLepton_narrow_validation/VRA4/dataTemplates/$sampleName\_MC.root
 done
 #Do reweight of GammaData and EWKSub MCs
 for sampleName in GammaData WJets-EWKSub WGamma-EWKSub TTJets-2lep-EWKSub TTJets-1lep-EWKSub SingleTop-EWKSub
 do
     echo "nohup ./ZMETLooper $sampleName configs/2017/DoubleLepton_narrow/dataTemplates/run_modes.conf > dataTemplates-$sampleName.out &"
-    nohup ./ZMETLooper $sampleName configs/2017/DoubleLepton_narrow/dataTemplates/run_modes.conf > dataTemplates-$sampleName.out &
+    nohup ./ZMETLooper $sampleName configs/2017/DoubleLepton_narrow/DoubleLepton_narrow_validation/VRA4/dataTemplates/run_modes.conf > dataTemplates-$sampleName.out &
 done
 
