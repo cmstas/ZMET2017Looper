f799df05e3c0b5abc8bc03c9df51f7bea59f9632
Balaji Sathia Narayanan bsathian@ucsd.edu 7/6/2018 - Updated Changed Username <bsathian@uaf-10.t2.ucsd.edu>
2019-07-30 17:00:38 -0700

    spring cleaning

--- DY_templates_Ht_test.sh
+++
@@ -1,5 +0,0 @@
-for SR in ZJets-0100 ZJets-100200 ZJets-200400 ZJets-400600 ZJets-600800 ZJets-8001200 ZJets-12002500 ZJets-2500Inf
-do
-    nohup ./ZMETLooper $SR configs/2017/Templates/closure/inclusive/run_modes.conf > ht_test-$SR.txt &
-
-done
