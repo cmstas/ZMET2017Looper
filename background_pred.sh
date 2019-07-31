06f242f0b41fb99844cf30f3871e5131faf765cd
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-07-30 18:23:17 -0700

    added vpt_rwt computation

--- background_pred.sh
+++ background_pred.sh
@@ -1,4 +1,8 @@
+#NOTE : I could've obviously added 3 years worth of stuff into a single TChain and sat back
+#but turns out with a little effort I can loop over every year separately and then combine whenever needed
+
 declare -a years=(2016 2017 2018)
+prefix="/home/users/bsathian/ZMet/histsthreeyears/"
 #Templates derivation
 echo "Running over gamma MC and Z MC"
 for year in "${years[@]}"
@@ -11,7 +15,26 @@ do
 done
 wait
 
+#DERIVING vpt_rwt
+#vpt_rwt needs to be done on 140 fb^-1 of data. So we combine three years
+#worth of histograms into a folder, hadd it, and send the combination back to reweight. Then we combine the reweighted GJets to get closure plots
+
 echo "Deriving vpt_rwt and making closure histograms"
+mkdir -p $prefix/MCTemplates/combined
+
+#hadd the copied stuff. HARDCODING THIS MOFO
+hadd -f -k $prefix/MCTemplates/combined/GammaJets.root $prefix/MCTemplates/2016/GammaJets.root $prefix/MCTemplates/2017/GammaJets.root $prefix/MCTemplates/2018/GammaJets.root
+hadd -f -k $prefix/MCTemplates/combined/ZJets.root $prefix/MCTemplates/2016/ZJets.root $prefix/MCTemplates/2017/ZJets.root $prefix/MCTemplates/2018/ZJets.root
+
+for year in "${years[@]}"
+do
+    for sampleName in ZJets GammaJets
+    do
+        cp $prefix/MCTemplates/combined/$sample.root $prefix/MCTemplates/$year/$sampleName-combined.root
+    done
+done
+
+#Now with the full histograms in the respective folders, do the vpt_rwt
 sampleName = GammaJets_reweight
 
 for year in "${years[@]}"
@@ -19,3 +42,4 @@ do
     echo "nohup ./ZMETLooper $sampleName configs/threeyears/MCTemplates/run_modes.conf $year zmet_datasets_$year.txt > MCTemplates-$sampleName-$year.out &"
     nohup ./ZMETLooper $sampleName configs/threeyears/MCTemplates/run_modes.conf $year zmet_datasets_$year.txt > MCTemplates-$sampleName-$year.out &
 done
+
