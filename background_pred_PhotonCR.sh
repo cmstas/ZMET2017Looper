970f89628c460f356836358d9b17395d67f6598d
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-07-31 18:39:32 -0700

    forgot the config file :

--- background_pred_PhotonCR.sh
+++ background_pred_PhotonCR.sh
@@ -4,7 +4,7 @@ prefix="/home/users/bsathian/ZMet/histsthreeyears"
 for year in "${years[@]}"
 do
     mkdir -p $prefix/PhotonCR/$year
-    for sampleName in GammaData WJets-EWKSub WGamma-EWKSub TTJets-2lep-EWKSub TTJets-1lep-EWKSub SingleTop-EWKSub
+    for sampleName in GammaData GammaJets WJets-EWKSub WGamma-EWKSub TTJets-2lep-EWKSub TTJets-1lep-EWKSub SingleTop-EWKSub
     do
 
         echo "nohup ./ZMETLooper $sampleName configs/threeyears/PhotonCR/run_modes.conf $year zmet_datasets_$year.txt > PhotonCR-$sampleName-$year.out &"
@@ -15,7 +15,7 @@ wait
 echo "Adding up three years worth of histograms"
 mkdir -p $prefix/PhotonCR/combined
 
-for sampleName in GammaData WJets-EWKSub WGamma-EWKSub TTJets-2lep-EWKSub TTJets-1lep-EWKSub SingleTop-EWKSub
+for sampleName in GammaData GammaJets WJets-EWKSub WGamma-EWKSub TTJets-2lep-EWKSub TTJets-1lep-EWKSub SingleTop-EWKSub
 do
     hadd -f -k $prefix/PhotonCR/combined/$sampleName.root $prefix/PhotonCR/2016/$sampleName.root $prefix/PhotonCR/2017/$sampleName.root $prefix/PhotonCR/2018/$sampleName.root
 
