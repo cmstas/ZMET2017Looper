48d67282cc09b6653167501b149f3c3bb68f3950
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-07-31 17:45:48 -0700

    New automation scripts

---
+++ background_pred_PhotonCR.sh
@@ -0,0 +1,22 @@
+declare -a years=(2016 2017 2018)
+prefix="/home/users/bsathian/ZMet/histsthreeyears"
+
+for year in "${years[@]}"
+do
+    mkdir -p $prefix/PhotonCR/$year
+    for sampleName in GammaData WJets-EWKSub WGamma-EWKSub TTJets-2lep-EWKSub TTJets-1lep-EWKSub SingleTop-EWKSub
+    do
+
+        echo "nohup ./ZMETLooper $sampleName configs/threeyears/PhotonCR/run_modes.conf $year zmet_datasets_$year.txt > PhotonCR-$sampleName-$year.out &"
+        nohup ./ZMETLooper $sampleName configs/threeyears/PhotonCR/run_modes.conf $year zmet_datasets_$year.txt > PhotonCR-$sampleName-$year.out &
+    done
+done
+
+echo "Adding up three years worth of histograms"
+mkdir -p $prefix/PhotonCR/combined
+
+for sampleName in GammaData WJets-EWKSub WGamma-EWKSub TTJets-2lep-EWKSub TTJets-1lep-EWKSub SingleTop-EWKSub
+do
+    hadd -f -k $prefix/PhotonCR/combined/$sampleName.root $prefix/PhotonCR/2016/$sampleName.root $prefix/PhotonCR/2017/$sampleName.root $prefix/PhotonCR/2018/$sampleName.root
+
+done
