d95c6d15a6424e0ab9ab4da851161828380148e0
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-07-31 19:16:19 -0700

    Added double lepton automation

---
+++ background_pred_DoubleLepton.sh
@@ -0,0 +1,24 @@
+declare -a years=(2016 2017 2018)
+prefix="/home/users/bsathian/ZMet/histsthreeyears/"
+sample=("Data" "DYJets" "TTJets" "SingleTop" "TTZ" "TTW" "WW" "WZTo2L2Q" "WZTo3LNu" "ZZ2L" "ZZ4L" "GluGluToZZ" "VVV")
+
+#Do a hadd for the EWK Sample to get the total Templates prediction
+mkdir -p $prefix/DoubleLepton/combined
+hadd -f -k $prefix/DoubleLepton/combined/Templates.root $prefix/dataTemplates/combined/GammaData.root $prefix/dataTemplates/combined/WGamma-EWKSub.root $prefix/dataTemplates/combined/WJets-EWKSub.root $prefix/dataTemplates/combined/TTJets-1lep-EWKSub.root $prefix/dataTemplates/combined/TTJets-2lep-EWKSub.root $prefix/dataTemplates/SingleTop-EWKSub.root
+
+for year in "${years[@]}"
+do
+    mkdir -p $prefix/DoubleLepton/$year
+    for sampleName in "${sample[@]}"
+    do
+        echo "nohup ./ZMETLooper $sampleName configs/threeyears/DoubleLepton/run_modes.conf $year zmet_datasets_$year.txt > DoubleLepton-$sampleName-$year.out &"
+        nohup ./ZMETLooper $sampleName configs/threeyears/DoubleLepton/run_modes.conf $year zmet_datasets_$year.txt > DoubleLepton-$sampleName-$year.out &
+    done
+done
+wait
+
+#hadd the DoubleLepton histograms
+for sampleName in "${sample[@]}"
+do
+    hadd -f -k $prefix/DoubleLepton/combined/$sampleName.root $prefix/DoubleLepton/2016/$sampleName.root $prefix/DoubleLepton/2017/$sampleName.root $prefix/DoubleLepton/2018/$sampleName.root
+done
