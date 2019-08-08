53d7011ba521fd9fcfb0aff505da511fa0eaabc2
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-08-07 19:53:15 -0700

    Added prescale weight shit

--- ScanChain.cc
+++ ScanChain.cc
@@ -1301,8 +1301,15 @@ double ZMETLooper::getWeight(TString SR){
 double ZMETLooper::getPrescaleWeight(){
   //cout<<__LINE__<<endl;
   //cout<<"Getting Prescale Weights"<<endl;
-  if(phys.HLT_Photon200() > 0 && phys.gamma_pt().at(0) > 210) return phys.HLT_Photon200();
-  else if(!phys.HLT_Photon165_R9Id90_HE10_IsoM_matchedtophoton() && phys.HLT_Photon165_R9Id90_HE10_IsoM() > 0  && phys.gamma_pt().at(0) > 180 && phys.gamma_pt().at(0) < 210) return phys.HLT_Photon165_R9Id90_HE10_IsoM();
+  if(phys.HLT_Photon200() > 0 && phys.gamma_pt().at(0) > 210 && g_year != 2016) return phys.HLT_Photon200();
+  else if(!phys.HLT_Photon165_R9Id90_HE10_IsoM_matchedtophoton() && phys.HLT_Photon165_R9Id90_HE10_IsoM() > 0  && phys.gamma_pt().at(0) > 180)
+  {
+      if(g_year == 2016)
+          return 1;
+    else if(phys.gamma_pt().at(0) < 210) 
+        return phys.HLT_Photon165_R9Id90_HE10_IsoM();
+  }
+  else if(phys.HLT_Photon165_HE10() > 0 && g_year == 2016) return 1;
   else if( !phys.HLT_Photon120_R9Id90_HE10_IsoM_matchedtophoton() && phys.HLT_Photon120_R9Id90_HE10_IsoM() > 0 && phys.gamma_pt().at(0) > 135 && phys.gamma_pt().at(0) < 180 ) return phys.HLT_Photon120_R9Id90_HE10_IsoM();
   else if( !phys.HLT_Photon90_R9Id90_HE10_IsoM_matchedtophoton() && phys.HLT_Photon90_R9Id90_HE10_IsoM()  > 0 && phys.gamma_pt().at(0) > 105 && phys.gamma_pt().at(0) < 135  ) return phys.HLT_Photon90_R9Id90_HE10_IsoM();
   else if( !phys.HLT_Photon75_R9Id90_HE10_IsoM_matchedtophoton() && phys.HLT_Photon75_R9Id90_HE10_IsoM()  > 0 && phys.gamma_pt().at(0) > 85 && phys.gamma_pt().at(0) < 105   ) return phys.HLT_Photon75_R9Id90_HE10_IsoM();
