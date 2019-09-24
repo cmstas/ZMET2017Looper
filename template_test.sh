dfecac302404ee7317685b7d99dc1436ceaa5242
Balaji Sathia Narayanan bsathian@ucsd.edu 7/6/2018 - Updated Changed Username <bsathian@uaf-8.t2.ucsd.edu>
2019-09-24 12:08:23 -0700

    pesky commits

--- template_test.sh
+++ template_test.sh
@@ -1,5 +1,6 @@
 SR=$1
-histsprefix="/home/users/bsathian/histstemplate_comparison/$SR/"
+year=2017
+histsprefix="/home/users/bsathian/ZMet/histstemplate_comparison/$SR/"
 for sample in cleanGamma cleanZ MCTemplates
 do
     echo "nohup ./ZMETLooper all configs/template_comparison/$SR/$sample/run_modes.conf 2017 zmet_datasets_2017.txt > template_comparison_$SR\_$sample.out &"
@@ -8,13 +9,15 @@ done
 wait
 
 #Add GammaData to create GammaData-EWKSub
-hadd -f -k $histsprefix/cleanGamma/GammaData-EWKSub.root $histsprefix/cleanGamma/GammaData.root $histsprefix/cleanGamma/WGamma-EWKSub.root $histsprefix/cleanGamma/WJets-EWKSub.root $histsprefix/cleanGamma/TTJets-2lep-EWKSub.root $histsprefix/cleanGamma/TTJets-1lep-EWKSub.root $histsprefix/cleanGamma/SingleTop-EWKSub.root
-mkdir -p $histsprefix/reweightMCGamma/
-cp $histsprefix/MCTemplates/ZJets.root $histsprefix/reweightMCGamma/ZJets-MC.root
-cp $histsprefix/MCTemplates/GammaJets.root $histsprefix/reweightMCGamma/GammaJets-MC.root
+hadd -f -k $histsprefix/cleanGamma/$year/GammaData-EWKSub.root $histsprefix/cleanGamma/$year/GammaData.root $histsprefix/cleanGamma/$year/WGamma-EWKSub.root $histsprefix/cleanGamma/$year/WJets-EWKSub.root $histsprefix/cleanGamma/$year/TTJets-2lep-EWKSub.root $histsprefix/cleanGamma/$year/TTJets-1lep-EWKSub.root $histsprefix/cleanGamma/$year/SingleTop-EWKSub.root
+
+#copy MCTemplates ZJets and GammaJets into reweightMCGamma folder
+mkdir -p $histsprefix/reweightMCGamma/$year/
+cp $histsprefix/MCTemplates/$year/ZJets.root $histsprefix/reweightMCGamma/$year/ZJets-MC.root
+cp $histsprefix/MCTemplates/$year/GammaJets.root $histsprefix/reweightMCGamma/$year/GammaJets-MC.root
 
 for sample in reweightGamma reweightMCGamma
 do
-    echo "nohup ./ZMETLooper all configs/template_comparison/$SR/$sample/run_modes.conf 2017 zmet_datsets_2017.txt > template_comparison_$SR\_$sample.out &"
-    nohup ./ZMETLooper all configs/template_comparison/$SR/$sample/run_modes.conf 2017 zmet_datsets_2017.txt > template_comparison_$SR\_$sample.out &
+    echo "nohup ./ZMETLooper all configs/template_comparison/$SR/$sample/run_modes.conf 2017 zmet_datasets_2017.txt > template_comparison_$SR\_$sample.out &"
+    nohup ./ZMETLooper all configs/template_comparison/$SR/$sample/run_modes.conf 2017 zmet_datasets_2017.txt > template_comparison_$SR\_$sample.out &
 done
