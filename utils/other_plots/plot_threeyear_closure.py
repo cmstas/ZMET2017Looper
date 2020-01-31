90869903e3980e0f9a1cd420b69d93494d10fa59
Balaji Sathia Narayanan bsathian@ucsd.edu 7/6/2018 - Updated Changed Username <bsathian@uaf-10.t2.ucsd.edu>
2020-01-31 05:17:01 -0800

    Updated template closure plotting script

--- utils/other_plots/plot_threeyear_closure.py
+++ utils/other_plots/plot_threeyear_closure.py
@@ -4,8 +4,8 @@ import ROOT as r
 import numpy as np
 import sys,os
 
-prefix = "/home/users/bsathian/ZMet/histsthreeyears/MCTemplates/combined/"
-output_prefix = "/home/users/bsathian/public_html/ZMET_plots/threeyears/MCTemplates/"
+prefix = "/home/users/bsathian/ZMet/histsthreeyears/strategy_B/loose_BVeto/MCTemplates/combined/"
+output_prefix = "/home/users/bsathian/public_html/ZMET_plots/threeyears/MCTemplates/strategy_B/loose_BVeto/"
 gammaFile = r.TFile(os.path.join(prefix,"GammaJets_reweight.root"))
 gammaFile_unweighted = r.TFile(os.path.join(prefix,"GammaJets.root"))
 ZFile = r.TFile(os.path.join(prefix,"ZJets.root"))
@@ -16,12 +16,32 @@ def integrate_histograms(hist):
     scale = hist.Integral(hist.FindBin(50),hist.FindBin(99.99))
     return scale
 
+SRC_binning = np.array([0,50,100,150,250,600],dtype = np.float64)
+SRVZBoosted_binning = np.array([0,50,100,600],dtype = np.float64)
+SRVZResolved_binning = np.array([0,50,100,150,250,600],dtype = np.float64)
+SRHZ_binning = np.array([0,50,100,150,600],dtype = np.float64)
+SRB_binning = np.array([0,50,100,150,230,600],dtype = np.float64)
 other_binning = np.array([0,50,100,150,230,300,600],dtype = np.float64)
 for SR in SRs:
     print("SR =",SR)
+    if "SRB" in SR or "VRB" in SR:
+        binning = SRB_binning
+    elif  "SRA" in SR  or "VRA" in SR:
+        binning = other_binning
+    elif "SRC" in SR or "VRC" in SR:
+        binning = SRC_binning
+    elif "Boosted" in SR:
+        binning = SRVZBoosted_binning
+    elif "Resolved" in SR:
+        binning = SRVZResolved_binning
+    else:
+        binning = SRHZ_binning
+#    gamma_hist = gammaFile.Get(SR + "type1MET_widebin"
     gamma_hist = gammaFile.Get(SR + "type1MET").Rebin(len(binning)-1,"rebin_met_mc_"+SR,binning)
     gamma_hist_unweighted = gammaFile_unweighted.Get(SR+"type1MET").Rebin(len(binning)-1,"rebin_met_mc_unsweighted_"+SR,binning)
+#    gamma_hist_unweighted = gammaFile_unweighted.Get(SR+"type1MET_widebin")
     Z_hist = ZFile.Get(SR+"type1MET").Rebin(len(binning)-1,"rebin_met_Z_mc_"+SR,binning)
+#    Z_hist = ZFile.Get(SR+"type1MET_widebin")
     gamma_scale = integrate_histograms(gamma_hist)
     gamma_unweighted_scale = integrate_histograms(gamma_hist_unweighted)
     Z_scale = integrate_histograms(Z_hist)
@@ -36,6 +56,7 @@ for SR in SRs:
                 "yaxis_log":True,
                 "output_name":output_prefix+SR+"_template_hists.pdf",
                 "xaxis_label":"Type 1 MET",
+                "title" : SR,
                 "legend_smart":False,
                 "legend_datalabel":"Z+Jets",
                 "xaxis_range":[50,600],
@@ -71,10 +92,17 @@ for SR in SRs:
 
 
     #Closure error computing
-    TH1F ratio_hist = Z_hist.Clone("gamma_hist_for_ratio")
+    ratio_hist = Z_hist.Clone("gamma_hist_for_ratio")
     ratio_hist.Divide(gamma_hist)
-    for i in range(1,len(binning)):
-        print("Bin {}-{} Ratio = {} Error = {}".format(binning[i],binning[i+1],ratio_hist.GetBinContent(i),ratio_hist.GetBinError(i)))
+    print("\\hline")
+    for i in range(2,len(binning)):
+        if i == 2:
+            SR_toprint = SR
+        else:
+            SR_toprint = ""
+        print("{} & {}-{} & ${:0.2f} \pm {:0.2f}$ & ${:0.2f} \pm {:0.2f}$ & ${:0.2f} \pm {:0.2f}$\\\\".format(SR_toprint,Z_hist.GetBinLowEdge(i),Z_hist.GetBinLowEdge(i+1),gamma_hist.GetBinContent(i),gamma_hist.GetBinError(i),Z_hist.GetBinContent(i),Z_hist.GetBinError(i),ratio_hist.GetBinContent(i),ratio_hist.GetBinError(i)))
+#        print("SR = {} Bin {}-{} Gamma = {} \pm {}, Z = {} \pm {}, Ratio = {} Closure Error = {}".format(SR,Z_hist.GetBinLowEdge(i),Z_hist.GetBinLowEdge(i+1),gamma_hist.GetBinContent(i), gamma_hist.GetBinError(i),Z_hist.GetBinContent(i), Z_hist.GetBinError(i),ratio_hist.GetBinContent(i),max(ratio_hist.GetBinError(i),abs(1-ratio_hist.GetBinContent(i)))))
+    print("\\hline")
 #Plot directory stuff
 os.system("chmod -R 755 "+output_prefix)
 os.system("sh ~/niceplots/niceplots.sh "+output_prefix)
